26a9df471dd6b8bd02154d0c2b1130120a667acc
Maik Jablonski <jablonski@neoskop.de>
2024-02-27 00:31:21 +0100

    added FastifyServer to types.d.ts

--- src/routes/types.d.ts
+++ src/routes/types.d.ts
@@ -2,6 +2,9 @@
 
 import { FastifyReply } from "fastify/types/reply";
 import { FastifyRequest } from "fastify/types/request";
+import serverless from "jeasx/serverless";
+
+type FastifyServer = typeof serverless;
 
 type RouteProps = {
   request: FastifyRequest;
