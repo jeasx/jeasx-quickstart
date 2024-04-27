c81658ca452b47b7f2386a00964dbcf046fcb241
Maik Jablonski <jablonski@neoskop.de>
2024-04-27 19:24:46 +0200

    "jeasx": "0.7.0"

--- src/routes/[...guard].js
+++ src/routes/[...guard].js
@@ -9,4 +9,17 @@ export default function RootGuard({ request, reply }) {
   // Set the request and reply objects in the request context
   requestContext.set("request", request);
   requestContext.set("reply", reply);
+
+  // Set optional response handler in request context.
+  // Below is an example of how to prettify resulting HTML.
+  /*
+  import * as prettier from "prettier";
+  ...
+  requestContext.set("response", async (payload) => {
+    return typeof payload === "string" &&
+      String(reply.getHeader("content-type")).startsWith("text/html")
+      ? await prettier.format(payload, { parser: "html" })
+      : payload;
+  });
+  */
 }
