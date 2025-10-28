7f7de92f14f110249ad3d141944e6f514c92645b
Maik Jablonski <jablonski@neoskop.de>
2025-10-28 12:01:39 +0100

    updated .env.js with more examples

--- .env.js
+++ .env.js
@@ -4,9 +4,13 @@ const NODE_ENV_IS_DEVELOPMENT = process.env.NODE_ENV === "development";
 
 export default {
   FASTIFY_DISABLE_REQUEST_LOGGING: NODE_ENV_IS_DEVELOPMENT,
-  FASTIFY_STATIC_HEADERS: NODE_ENV_IS_DEVELOPMENT
-    ? { "": { "Cache-Control": "no-store" } }
-    : {
-        "": { "Cache-Control": "public,max-age=31536000,s-maxage=31536000" },
-      },
+  // FASTIFY_REWRITE_URL: (/** @type import("fastify").FastifyRequest */ req) =>
+  //   req.url.replace(/^\/jeasx/, ""),
+  // FASTIFY_STATIC_HEADERS: NODE_ENV_IS_DEVELOPMENT
+  //   ? {
+  //       "": { "Cache-Control": "public,max-age=60" },
+  //       ".js": { "Cache-Control": "no-store" },
+  //       ".css": { "Cache-Control": "no-store" },
+  //     }
+  //   : { "": { "Cache-Control": "public,max-age=31536000,s-maxage=31536000" } },
 };
