68dea43c09648969f4936c9c541c8f48db290308
Maik Jablonski <jablonski@neoskop.de>
2025-11-13 11:02:24 +0100

    cleaned up .env*

--- .env.js
+++ .env.js
@@ -2,8 +2,10 @@
 
 const NODE_ENV_IS_DEVELOPMENT = process.env.NODE_ENV === "development";
 
+// https://www.jeasx.dev/configuration
 export default {
   FASTIFY_DISABLE_REQUEST_LOGGING: NODE_ENV_IS_DEVELOPMENT,
+  // FASTIFY_BODY_LIMIT: 1048576,
   // FASTIFY_REWRITE_URL: (/** @type import("fastify").FastifyRequest */ req) =>
   //   req.url.replace(/^\/jeasx/, ""),
   // FASTIFY_STATIC_HEADERS: NODE_ENV_IS_DEVELOPMENT
@@ -13,4 +15,6 @@ export default {
   //       ".css": { "Cache-Control": "no-store" },
   //     }
   //   : { "": { "Cache-Control": "public,max-age=31536000,s-maxage=31536000" } },
+  // ESBUILD_BROWSER_TARGET: "chrome130,edge130,firefox130,safari18",
+  // JEASX_ROUTE_CACHE_LIMIT: 10000,
 };
