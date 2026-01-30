bbea0b10dea3a6f219ef15fe6197e8b7bc7ac1f1
Maik Jablonski <jablonski@neoskop.de>
2026-01-30 10:22:51 +0100

    "jeasx": "2.3.0"

--- .env.js
+++ .env.js
@@ -4,19 +4,23 @@ const NODE_ENV_IS_DEVELOPMENT = process.env.NODE_ENV === "development";
 
 // https://www.jeasx.dev/configuration
 export default {
-  // ESBUILD_BROWSER_TARGET: "chrome130,edge130,firefox130,safari18",
+  /** @type import("esbuild").BuildOptions["target"] */
+  //ESBUILD_BROWSER_TARGET: ["chrome130", "edge130", "firefox130", "safari18"],
+
+  /** @type import("@mdx-js/esbuild").Options */
+  // ESBUILD_MDX_OPTIONS: {},
 
   /** @type import("fastify").FastifyServerOptions */
   FASTIFY_SERVER_OPTIONS: {
     disableRequestLogging: NODE_ENV_IS_DEVELOPMENT,
-    bodyLimit: 2 * 1024 * 1024,
-    // rewriteUrl: (req) => String(req.url).replace(/^\/jeasx/, ""),
+    bodyLimit: 2 * 1024 * 1024
+    // rewriteUrl: (req) => String(req.url).replace(/\.html(?=\?|$)/, ""),
   },
 
   /** @type import("@fastify/static").FastifyStaticOptions */
   FASTIFY_STATIC_OPTIONS: {
-    maxAge: NODE_ENV_IS_DEVELOPMENT ? 0 : "365d",
-  },
+    maxAge: NODE_ENV_IS_DEVELOPMENT ? 0 : "365d"
+  }
 
   /** @type import("@fastify/cookie").FastifyCookieOptions */
   // FASTIFY_COOKIE_OPTIONS: {},
