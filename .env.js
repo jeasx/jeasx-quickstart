052ab943d5a42ebb68e15fbd4189252c4bedf684
Maik Jablonski <jablonski@neoskop.de>
2026-02-20 11:49:18 +0100

    "jeasx": "2.4.0"

--- .env.js
+++ .env.js
@@ -4,30 +4,33 @@ const NODE_ENV_IS_DEVELOPMENT = process.env.NODE_ENV === "development";
 
 // https://www.jeasx.dev/configuration
 export default {
-  /** @type import("esbuild").BuildOptions["target"] */
-  //ESBUILD_BROWSER_TARGET: ["chrome130", "edge130", "firefox130", "safari18"],
+  /** @type {() => import("esbuild").BuildOptions} */
+  // ESBUILD_SERVER_OPTIONS: () => ({}),
 
-  /** @type import("@mdx-js/esbuild").Options */
-  // ESBUILD_MDX_OPTIONS: {},
+  /** @type {() => import("esbuild").BuildOptions} */
+  // ESBUILD_BROWSER_OPTIONS: () => ({
+  //   target: ["chrome130", "edge130", "firefox130", "safari18"],
+  // }),
 
-  /** @type import("fastify").FastifyServerOptions */
-  FASTIFY_SERVER_OPTIONS: {
+  /** @type {(fastify: import("fastify").FastifyInstance) => import("fastify").FastifyInstance} */
+  // FASTIFY_SERVER: (fastify) => fastify,
+
+  /** @type {() => import("fastify").FastifyServerOptions} */
+  FASTIFY_SERVER_OPTIONS: () => ({
     disableRequestLogging: NODE_ENV_IS_DEVELOPMENT,
-    bodyLimit: 2 * 1024 * 1024,
-    // rewriteUrl: (req) => String(req.url).replace(/\.html(?=\?|$)/, ""),
-  },
+  }),
 
-  /** @type import("@fastify/static").FastifyStaticOptions */
-  FASTIFY_STATIC_OPTIONS: {
+  /** @type {() => import("@fastify/static").FastifyStaticOptions} */
+  FASTIFY_STATIC_OPTIONS: () => ({
     maxAge: NODE_ENV_IS_DEVELOPMENT ? 0 : "365d",
-  },
+  }),
 
-  /** @type import("@fastify/cookie").FastifyCookieOptions */
-  // FASTIFY_COOKIE_OPTIONS: {},
+  /** @type {() => import("@fastify/cookie").FastifyCookieOptions} */
+  // FASTIFY_COOKIE_OPTIONS: () => ({}),
 
-  /** @type import("@fastify/formbody").FastifyFormbodyOptions */
-  // FASTIFY_FORMBODY_OPTIONS: {},
+  /** @type {() => import("@fastify/formbody").FastifyFormbodyOptions} */
+  // FASTIFY_FORMBODY_OPTIONS: () => ({}),
 
-  /** @type import("@fastify/multipart").FastifyMultipartOptions */
-  // FASTIFY_MULTIPART_OPTIONS: {},
+  /** @type {() => import("@fastify/multipart").FastifyMultipartOptions} */
+  // FASTIFY_MULTIPART_OPTIONS: () => ({}),
 };
